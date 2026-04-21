43e8442e485fc44c83a0394f72cc6dac3153ee87
Matimu Khosa <2716924@students.wits.ac.za>
2026-04-21 20:45:57 +0200

    Dropped rows with embarked as null because they were few entries

--- junior.py
+++ junior.py
@@ -6,18 +6,22 @@ data = pd.read_csv("Titanic-Dataset.csv")
 
 
 df = pd.DataFrame(data)
-missing = pd.isnull(df)
-print(missing.sum()) # data before using the imputer
+print(len(df))
+
+missing = pd.isnull(df["Embarked"])
+print(missing.sum()) #number of missing value
+df =  df[~missing]
+print(len(df))
 
 
 #using the imputer
-#imputer = KNNImputer(n_neighbors=5) # apparently risk of overfitting with more neighbors but we will see how it goes
-#age_after = imputer.fit_transform(df[["Age"]])
-#df["Age"] = age_after
-#print(age_after)
+# imputer = KNNImputer(n_neighbors=5) # apparently risk of overfitting with more neighbors but we will see how it goes
+# age_after = imputer.fit_transform(df[["Age"]])
+# df["Age"] = age_after
+# #print(age_after)
 
-#using the median of sex, class and age to fill in the values for the ages
-#df["Age"] = df.groupby(["Sex", "Pclass"])["Age"].transform(lambda x: x.fillna(x.mean()))
-#print(df["Age"])
+# #using the median of sex, class and age to fill in the values for the ages
+# df["Age"] = df.groupby(["Sex", "Pclass"])["Age"].transform(lambda x: x.fillna(x.mean()))
+# #print(df["Age"])
 
-#print(len(df[(df["Sex"] == "female") & (df["Survived"] == 1)]))
+# print(len(df[(df["Sex"] == "female") & (df["Survived"] == 1)]))
